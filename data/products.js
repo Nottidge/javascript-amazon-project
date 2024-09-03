8b8df40cb1588fae3b807cdfdd58710e1ab6b007
Nottidge Chibueze <nottidgechibueze042@gmail.com>
2024-09-03 18:49:01 +0100

    Load products from the backend

--- data/products.js
+++ data/products.js
@@ -36,6 +36,26 @@ class Product {
   }
 }
 
+export let products = [];
+
+export function loadProducts (fun) {
+  const xhr = new XMLHttpRequest();
+
+  xhr.addEventListener('load', () => {
+    products = JSON.parse(xhr.response).map((productDetails) => {
+      return new Product(productDetails);
+   });
+
+   console.log('load products');
+
+   fun();
+  });
+
+  xhr.open('GET', 'https://supersimplebackend.dev/products');
+  xhr.send();
+}
+
+/*
 export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
@@ -698,3 +718,4 @@ export const products = [
 ].map((productDetails) => {
    return new Product(productDetails);
 });
+*/
